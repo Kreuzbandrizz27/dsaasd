8a95e88e91853d435ac7e9fa741d96d199b33962
Kreuzbandrizz27 <kayhansatrox2005@gmail.com>
2025-08-26 18:54:33 +0200

    Add files via upload

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+// eslint.config.js
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginReactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  pluginReactConfig,
+  {
+    plugins: {
+      "react-hooks": pluginReactHooks,
+      "react-refresh": pluginReactRefresh,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": "warn",
+      "react/react-in-jsx-scope": "off", // Not needed with modern React/Vite
+    },
+  },
+];
